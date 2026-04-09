4e41ced84c87cc2b23d818683b0a774b063232a1
Tenth <education@tenth8602.org>
2026-04-09 09:02:18 -0700

    TN plugin baseline

--- src/main/java/me/tenth8602/tN/EventListener.java
+++ src/main/java/me/tenth8602/tN/EventListener.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 
@@ -13,9 +12,7 @@ public class EventListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         player.sendMessage("<green><b>Welcome to Tenth Network");
-    }
-    @EventHandler
-    public void onLeave(PlayerKickEvent event) {
-        Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " got kicked");
+        player.sendMessage("<newline><red><b>This server is under development. transfering");
+        player.transfer("node-1.ryzenclouds.online",25572);
     }
 }
